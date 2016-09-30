e5eeedee5147197b3493fe76628e37583c422c4e
Phil Ewels <phil.ewels@scilifelab.se>
2016-09-30 09:34:12 +0200

    Testing and getting script to work.

--- fetch_stats.py
+++ fetch_stats.py
@@ -8,22 +8,22 @@ downstream use.
 All jobstats are saved as files in /sw/share/slurm/<cluster>/uppmax_jobstats/
 for 30 days. Martin Dahlo then saves all of these permanently into three databases:
 
-*  /proj/b2013023/statistics/general/general.sqlite
-  > contains general info about jobs and projects - this is probably where
-  > you could find your data to filter on.
-  > cluster and jobid make a primary key for individual jobs
+* /proj/b2013023/statistics/general/general.sqlite
+    > contains general info about jobs and projects - this is probably where
+    > you could find your data to filter on.
+    > cluster and jobid make a primary key for individual jobs
 
-*  /proj/b2013023/statistics/job_efficiency/job_efficiency.sqlite
-  > processed efficiency values, that summarize each job to a few numbers, but
-  > not a timeline like in the jobstats plots.  It can be used to quickly get the efficiency info.
+* /proj/b2013023/statistics/job_efficiency/job_efficiency.sqlite
+    > processed efficiency values, that summarize each job to a few numbers, but
+    > not a timeline like in the jobstats plots.  It can be used to quickly get the efficiency info.
 
-*  /proj/b2013023/statistics/job_stats_raw/stats
-  > contains the raw jobstats data which can be used to do the timeline
-  > these stats are in a slightly more complex format though - the database is sharded into one
-  > database per node since there are so many jobs ant the raw data is quite fluffy. It was very
-  > slow to have them all in a single sqlite db so i created a sqlite file for each node.
-  > They are all identical in structure inside so you'll have to have the node name the job ran on
-  > (from general.sqlite), then open a connection to it and query the db
+* /proj/b2013023/statistics/job_stats_raw/stats
+    > contains the raw jobstats data which can be used to do the timeline
+    > these stats are in a slightly more complex format though - the database is sharded into one
+    > database per node since there are so many jobs ant the raw data is quite fluffy. It was very
+    > slow to have them all in a single sqlite db so i created a sqlite file for each node.
+    > They are all identical in structure inside so you'll have to have the node name the job ran on
+    > (from general.sqlite), then open a connection to it and query the db
 
 Author: Phil Ewels <phil.ewels@scilifelab.se>
 Started: 29-09-2016
@@ -47,16 +47,16 @@ config = {
 @click.command(
     context_settings = dict( help_option_names = ['-h', '--help'] )
 )
-@click.argument('projects',
+@click.option('-A', '--project', 'projects',
     type = str,
-    nargs = -1,
+    multiple = True,
     required = True,
-    metavar = "<project IDs>"
+    help = "UPPMAX Project ID to filter for. You can specify multiple projects by repeating this flag."
 )
 @click.option('-d', '--days',
     type = int,
     default = 14,
-    metavar = "<days previous to fetch>"
+    help = "Number of days' statistics to fetch, counted back from now. Default: 14"
 )
 @click.option('-q', '--quiet',
     is_flag = True,
@@ -92,7 +92,7 @@ def fetch_stats(projects, days, quiet, verbose):
     # Fetch jobs from this project
     timestamp = int(time.time())
     since = timestamp - (days*24*60*60)
-    general_query = "SELECT * from jobs WHERE start > {} and proj_id IN ({})".format(since, ",".join(projects))
+    general_query = "SELECT * from jobs WHERE start > {} and proj_id IN ('{}')".format(since, "','".join(projects))
     logging.debug("Running query: {}".format(general_query))
     cursor = general_conn.execute(general_query)
     for row in cursor:
@@ -127,27 +127,35 @@ def fetch_stats(projects, days, quiet, verbose):
     # Get job summary stats - NB: Job IDs not unique across different clusters!
     # list_tables(efficiency_conn)
     i = 1
+    count_none = 0
     for id in jobs:
-        logging.debug("Getting summary stats for job {} of {}".format(i, len(jobs)))
+        if i % 100 == 0:
+          logging.debug("Getting summary stats for job {} of {}".format(i, len(jobs)))
         i += 1
         efficiency_query = "SELECT * from jobs WHERE job_id = '{}' AND proj_id = '{}' AND user = '{}'".format(id, jobs[id]['proj_id'], jobs[id]['user'])
         # logging.debug("Running query: {}".format(efficiency_query))
         cursor = efficiency_conn.execute(efficiency_query)
         row = cursor.fetchone()
-        # jobs[id]['job_id'] = row[0]
-        # jobs[id]['cluster'] = row[1]
-        # jobs[id]['proj_id'] = row[2]
-        jobs[id]['user'] = row[3]
-        jobs[id]['cpu_mean'] = row[4]
-        jobs[id]['cpu_cores_used_percentile'] = row[5]
-        # jobs[id]['cores'] = row[6]
-        jobs[id]['mem_peak'] = row[7]
-        jobs[id]['mem_median'] = row[8]
-        jobs[id]['mem_limit'] = row[9]
-        jobs[id]['counts'] = row[10]
-        # jobs[id]['state'] = row[11]
-        jobs[id]['date_finished'] = row[12]
-        # jobs[id]['nodelist'] = row[13]
+        try:
+            # jobs[id]['job_id'] = row[0]
+            # jobs[id]['cluster'] = row[1]
+            # jobs[id]['proj_id'] = row[2]
+            jobs[id]['user'] = row[3]
+            jobs[id]['cpu_mean'] = row[4]
+            jobs[id]['cpu_cores_used_percentile'] = row[5]
+            # jobs[id]['cores'] = row[6]
+            jobs[id]['mem_peak'] = row[7]
+            jobs[id]['mem_median'] = row[8]
+            jobs[id]['mem_limit'] = row[9]
+            jobs[id]['counts'] = row[10]
+            # jobs[id]['state'] = row[11]
+            jobs[id]['date_finished'] = row[12]
+            # jobs[id]['nodelist'] = row[13]
+        except TypeError:
+            count_none += 1
+            logging.debug("No results found for job {} ({}, {})".format(id, jobs[id]['proj_id'], jobs[id]['user']))
+    
+    logging.info("Found results for {} out of {} jobs".format(i-count_none, i))
     
     # Close efficiency stats database
     efficiency_conn.close()
